110c381c1f86e95380ee688af8c040d87ec6ffaa
lzc <1244315824@qq.com>
2021-03-08 00:04:26 +0800

    update

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -48,5 +48,6 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(viewModel: EmojiMemoryGame())
+            
     }
 }
