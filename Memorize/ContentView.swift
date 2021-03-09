9f486182ee0c34dab0a39ab8dd34a805aed60443
lzc <zuchuang.liu@melot.cn>
2021-03-09 16:13:11 +0800

    Layout udpate

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -20,7 +20,6 @@ struct ContentView: View {
         }
         .padding()
         .foregroundColor(.orange)
-        .font(.largeTitle)
     }
 }
 
@@ -29,18 +28,31 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader{geometry in
+            self.body(size: geometry.size)
+        }
+    }
+    
+    func body(size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .fill(Color.white)
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(lineWidth: lineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .fill()
             }
         }
+        .font(.system(size: systemFont(size: size)))
+    }
+    
+    let cornerRadius: CGFloat = 10
+    let lineWidth: CGFloat = 3
+    func systemFont(size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
     }
 }
 
